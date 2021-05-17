7a2e146a826a6f8a40c4317ce3cac038f574dfbe
NikoriakViktot <83222767+NikoriakViktot@users.noreply.github.com>
2021-05-17 16:54:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+
+
+# task 1
+
+
+class Animal:
+
+    def talk(self):
+     
+
+
+
+
+
+class Dog(Animal):
+    def talk(self):
+        print ('Гав')
+
+
+
+class Cat(Animal):
+    def talk(self):
+       print ('Мяу')
+
+
+
+
+obj1 = Dog()
+obj2 = Cat()
+
+#obj1.talk()
+#obj2.talk()
+
+
+# task 2
+
+
+
